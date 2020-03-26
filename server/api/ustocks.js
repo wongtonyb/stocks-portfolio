2b85eaa3e7485dfe16641ab24b2451176714c38d
Tony <wongtonyb@gmail.com>
2020-03-25 21:35:27 -0400

    lowercased sequelize

--- server/api/ustocks.js
+++ server/api/ustocks.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {Op} = require('Sequelize')
+const {Op} = require('sequelize')
 const {Ustocks, User} = require('../db/models')
 module.exports = router
 
